b8aa6dcf3d01af9aa1f4c99aaac0d2821967b5cc
Ellie Sargent <elliecsargent@gmail.com>
2022-08-16 17:22:50 -0500

    add replies completed

--- models/Comment.js
+++ models/Comment.js
@@ -1,58 +1,59 @@
 const { Schema, model, Types } = require('mongoose');
-const { STRING } = require('sequelize/types');
 const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
-    {
-        // set custom id to avoid confusion with parent comment _id
-        replyId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        replyBody: {
-            type: STRING
-         },
-        writtenBy: {
-            type: String
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        }
+  {
+    // set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
     },
-    {
-        toJSON: {
-            getters: true
-        }
-    }
-);
-
-const CommentSchema = new Schema({
+    replyBody: {
+      type: String
+    },
     writtenBy: {
-        type: String
-    },
-    commentBody: {
-        type: String
+      type: String
     },
     createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
-    },
-    replies: [ReplySchema]
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
     }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
 );
 
-CommentSchema.virtual('replayCount').get(function() {
-    return this.replies.length;
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String
+    },
+    commentBody: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
